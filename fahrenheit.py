85a4f005cf1a40294ee88e0f7ad4c1beeda181e6
tanishka-10 <138995203+tanishka-10@users.noreply.github.com>
2023-07-12 14:19:35 -0400

    Delete fahrenheit.py

--- fahrenheit.py
+++
@@ -1,8 +0,0 @@
-cel_temp = int(input("What is the temperature in celsius?"))
-def fahrenheit(cel_temp):
-    return(18 * cel_temp + 320) / 10
-
-
-print("The Fahrenheit equivalent of " + str(cel_temp) + " degrees Celsius is " + str(fahrenheit(cel_temp)) + ".")
-
-
