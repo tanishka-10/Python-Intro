990949cfb2596f11dd615e5ec196962b7a090888
tanishka-10 <138995203+tanishka-10@users.noreply.github.com>
2023-07-12 14:15:47 -0400

    Delete Monty Python.py

--- Monty Python.py
+++
@@ -1,6 +0,0 @@
-name = input("What is your name?")
-quest = input("What is your quest?")
-fav_color = input("What is your favorite color?")
-print( "So your name is " + name + ", your quest is " + quest + ", and your favorite color is " + fav_color + ".")
-
-
