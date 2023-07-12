fb4ac294fd2a423cc91fa83482c3ab136a11d536
tanishka-10 <138995203+tanishka-10@users.noreply.github.com>
2023-07-12 14:19:43 -0400

    Delete escape sequences.py

--- escape sequences.py
+++
@@ -1,9 +0,0 @@
-float_val = 0.55
-print(type(float_val))
-print(str(float_val) + " is a float.")
-print("\"Hello, I'm Tanishka, it's nice to meet you!\"")
-print("*******\n *****\n  ***\n   *")
-
-
-
-
