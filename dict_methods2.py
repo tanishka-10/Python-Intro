d6bfa5473494183268bf3bed640d173b7bcab627
tanishka-10 <138995203+tanishka-10@users.noreply.github.com>
2023-07-12 14:20:01 -0400

    Delete dict_methods2.py

--- dict_methods2.py
+++
@@ -1,7 +0,0 @@
-for key, value in {}.fromkeys("bcdfghjklmnpqrstvwxyz", "consonant").items():
-    print(key, value)
-fast_food = {"McDonald's": "Big Mac", "Burger King": "Whopper", "Chick-fil-A": "Original Chicken Sandwich"}
-print(fast_food.pop("McDonald's"))
-
-fast_food.popitem()
-print(fast_food)
