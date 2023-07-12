e5d0f246e4489f7ec6011245f899abdeb4ed1eb2
tanishka-10 <138995203+tanishka-10@users.noreply.github.com>
2023-07-12 14:19:51 -0400

    Delete dict_methods3.py

--- dict_methods3.py
+++
@@ -1,7 +0,0 @@
-internet_celebrities = {"DrDisrespect": "YouTube", "ZLaner": "Facebook", "Ninja": "Mixer"}
-another_one = {"shroud": "Twitch"}
-internet_celebrities.update(another_one)
-influencers = internet_celebrities.copy()
-internet_celebrities.clear()
-print(internet_celebrities)
-print(influencers)
