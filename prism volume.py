87b62cc97f7b9f0c7a1c94ef6002cb4b1214d845
tanishka-10 <138995203+tanishka-10@users.noreply.github.com>
2023-07-12 14:17:45 -0400

    Delete prism volume.py

--- prism volume.py
+++
@@ -1,8 +0,0 @@
-length = int(input("What is the length?"))
-width = int(input("What is the width?"))
-height = int(input("What is the height?"))
-def volume(length, width, height):
-    return(length * width * height)
-
-
-print(str("The volume of the rectangular prism is " +  str(volume(length, width, height)) + " cubic feet."))
